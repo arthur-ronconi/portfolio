4b5a2eb356605226948cfc98c7adc8825d4fffff
Arthur Ronconi <arthur.ronconi@gmail.com>
2020-10-21 17:37:33 -0300

    gtag

--- components/seo.jsx
+++ components/seo.jsx
@@ -13,7 +13,7 @@ export default function Seo() {
       />
 
       <meta property="og:type" content="website" />
-      <meta property="og:url" content="https://metatags.io/" />
+      <meta property="og:url" content="https://artu.dev/" />
       <meta
         property="og:title"
         content="Arthur Ronconi - Front End Design/Development"
@@ -22,13 +22,10 @@ export default function Seo() {
         property="og:description"
         content="I'm a Front End Designer/Developer and this is my porfolio page."
       />
-      <meta
-        property="og:image"
-        content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png"
-      />
+      <meta property="og:image" content="https://artu.dev/og.png" />
 
       <meta property="twitter:card" content="summary_large_image" />
-      <meta property="twitter:url" content="https://metatags.io/" />
+      <meta property="twitter:url" content="https://artu.dev/" />
       <meta
         property="twitter:title"
         content="Arthur Ronconi - Front End Design/Development"
@@ -37,10 +34,7 @@ export default function Seo() {
         property="twitter:description"
         content="I'm a Front End Designer/Developer and this is my porfolio page."
       />
-      <meta
-        property="twitter:image"
-        content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png"
-      />
+      <meta property="twitter:image" content="https://artu.dev/og.png" />
     </Head>
   );
 }
